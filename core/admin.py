bc5ce657afe0c5abef6f02e27cc5cd0df1534a67
Sudin Rupakheti <sudinrupakheti977@gmail.com>
2026-04-09 16:09:28 +0545

    add: core models - MenuItem, Order, OrderItem, DishRating

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MenuItem, Order, OrderItem, DishRating
 
-# Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(DishRating)
